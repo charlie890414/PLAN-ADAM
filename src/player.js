be9e0e840c5d8bd2433c181ccd86efc1ad03725b
David Day <dj6082013@gmail.com>
2019-10-17 02:10:46 +0800

    fix keyboard listen

--- src/player.js
+++ src/player.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+const Keyboard = require('pixi.js-keyboard');
 
 export default class extends PIXI.Sprite {
     constructor() {
@@ -20,6 +21,24 @@ export default class extends PIXI.Sprite {
             const mouse = app.renderer.plugins.interaction.mouse.global;
             var position = this.toGlobal(panel);
             this.rotation = Math.atan2(position.y - mouse.y, position.x - mouse.x);
+
+            this.walk();
+            Keyboard.update();
         });
+    }
+
+    walk() {
+        const speed = 5;
+
+        // Keyboard
+        if (Keyboard.isKeyDown('ArrowLeft', 'KeyA'))
+            this.x -= speed;
+        if (Keyboard.isKeyDown('ArrowRight', 'KeyD'))
+            this.x += speed;
+
+        if (Keyboard.isKeyDown('ArrowUp', 'KeyW'))
+            this.y -= speed;
+        if (Keyboard.isKeyDown('ArrowDown', 'KeyS'))
+            this.y += speed;
     }
 }
