9da0a2474f3705921089f730eecfd91c36710122
cliffxzx <cliffxzx@gmail.com>
2019-10-20 01:06:24 +0800

    Add dropdown

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import HUD from './hud';
 import Welcome from './welcome';
 import DrawPlanet from './draw';
 import * as $ from 'jquery';
+import 'bootstrap';
 
 function startGame() {
 
@@ -69,5 +70,6 @@ $('.elm').each((idx, el)=> {
     })
 })
 
+$('.dropdown-toggle').dropdown();
 
 Welcome.welcome(startGame);
